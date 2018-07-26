970b00bb9c34d687b66c71a54b0c1d9717723b09
connie <connie@conniedeMacBook-Pro.local>
2018-07-26 08:20:33 +0800

    add form

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask,render_template
 from flask_bootstrap  import  Bootstrap
+from nameForm import NameForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'colin'
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+
+@app.route('/',  methods=['GET', 'POST'])
 def index():
-    return render_template('base.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('user.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(Debug=True)
